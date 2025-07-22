173735ce26988e35ad46380757303552d2d4a733
Folombas <folombas@yandex.ru>
2025-07-22 22:18:51 +0300

    This is Lua

---
+++ hello.lua
@@ -0,0 +1,10 @@
+io.write("Please enter your name: ")
+local name = io.read()
+print("Hello, " .. name .. "!")
+print([[
+    ====================================
+    ||                               ||
+    ||        This is Lua!           ||
+    ||                               ||
+    ====================================
+]])
